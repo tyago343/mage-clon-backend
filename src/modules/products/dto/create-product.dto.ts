c1763d32086e888b31919f88c103a1271177ca56
Santiago Casanova <tyagocasanova@gmail.com>
2022-12-03 03:12:49 +0100

    Changing dto for product

--- src/modules/products/dto/create-product.dto.ts
+++ src/modules/products/dto/create-product.dto.ts
@@ -1,4 +1,4 @@
-import { IsString } from 'class-validator';
+import { IsArray, IsString } from 'class-validator';
 import { Category } from 'src/modules/categories/entities';
 
 export class CreateProductDto {
@@ -8,5 +8,6 @@ export class CreateProductDto {
   price: string;
   @IsString()
   description: string;
+  @IsArray()
   categories: any;
 }
